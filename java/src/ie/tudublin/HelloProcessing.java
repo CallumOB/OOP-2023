27bd0136d485e5263aa90f19c38e1daf028e4a30
CallumOB <callumobrien2004@gmail.com>
2023-01-30 12:41:55 +0000

    All-Seeing Eye

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -12,7 +12,7 @@ public class HelloProcessing extends PApplet
 
 	public void setup() {
 		// colorMode(HSB);
-		background(0);
+		background(255, 0, 0);
 
 		
 		
@@ -20,15 +20,26 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-		stroke(255);
-		line(10, 10, 100, 100); // X1, Y1, X2, Y2
-		fill(255, 255, 0);
-		circle(300, 250, 70); // Position (X, Y), Size
-		fill(255, 0, 255);
+		// stroke(255);
+		// line(10, 10, 100, 100); // X1, Y1, X2, Y2
+		// fill(255, 255, 0);
+		// circle(300, 250, 70); // Position (X, Y), Size
+		// fill(255, 0, 255);
+		// noStroke();
+		// rect(10, 300, 20, 100); // X1, Y1, width, height
+		// stroke(127);
+		// fill(0, 0, 237);
+		// triangle(40, 90, 300, 20, 80, 70);
+
 		noStroke();
-		rect(10, 300, 20, 100); // X1, Y1, width, height
-		stroke(127);
-		fill(0, 0, 237);
-		triangle(40, 90, 300, 20, 80, 70);
+		
+		fill(255, 255, 0);
+		circle(width / 2, (height / 2) + 15, width - 30);
+		fill(0, 255, 255);
+		triangle(30, 450, width / 2 , 10, width - 30, 450);
+		fill(255);
+		ellipse((width / 2), (height / 2) - 50, 185, 100);
+		fill(0);
+		circle((width / 2), (height / 2) - 50, mouseX / 2);
 	}
 }
