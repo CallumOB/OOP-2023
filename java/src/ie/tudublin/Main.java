d8664ed549c0997b217585cc5abb9019a2e5a73e
CallumOB <callumobrien2004@gmail.com>
2023-02-13 11:26:19 +0000

    Setup branch

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,33 +3,13 @@ package ie.tudublin;
 public class Main
 {
 
-	public static void helloProcessing()
-	{
+	public static void starMap() {
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
+        processing.core.PApplet.runSketch( a, new StarMap());
+	}
 
-	public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-
-	public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-	
-	public static void main(String[] args)
-	{
-		System.out.println("Hello world");
-		
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
-
-		loops();
+	public static void main(String[] args) {
+		starMap();
 	}
 	
 }
---
+++ java/src/ie/tudublin/StarMap.java
@@ -0,0 +1,19 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class StarMap extends PApplet {
+
+	public void settings() {
+		size(750, 750);
+	}
+
+	public void setup() {
+		colorMode(HSB);
+		background(0);
+	}
+
+	public void draw() {
+
+	}
+}
