45e490198cbb09c60d99acb56c04c2d9ce0f72a9
CallumOB <callumobrien2004@gmail.com>
2023-02-21 13:40:59 +0000

    Lab working, missed this lab so annotated given code.

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -1,16 +1,21 @@
 package ie.tudublin;
 
-import processing.data.Table;
 import processing.data.TableRow;
 import processing.core.PApplet;
-import java.util.ArrayList;
 
 public class Star {
-    boolean hab;
-    String displayName;
-    float distance;
-    float xG, yG, zG, absMag;
-    ArrayList<Star> stars = new ArrayList<Star>();
+    private boolean hab; // Habitability 
+    private String displayName; // The display name of the planet
+    private float distance; // Distance from the sun in parsecs
+    private float xG, yG, zG; // X, Y, Z galactic cartesian coordinates in parsecs 
+    private float absMag; // Star's magnitude (size)
+
+    // This code overrides the standard output method so that if the Star object is printed, it will have a very specific formatted output.
+    @Override
+    public String toString() {
+        return "Star [absMag=" + absMag + ", displayName=" + displayName + ", distance=" + distance + ", hab=" + hab
+                + ", xG=" + xG + ", yG=" + yG + ", zG=" + zG + "]";
+    }
 
     public Star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag) {
         this.hab = hab;
@@ -35,11 +40,66 @@ public class Star {
  		);
  	}
 
-    void loadStars() {
-        Table table = loadTable("HabHYG15ly.csv", "header");
-        for (TableRow r : table.rows()) {
-            Star s = new Star(r);
-            stars.add(s);
-        }
+    public boolean isHab() {
+        return hab;
     }
+    public void setHab(boolean hab) {
+        this.hab = hab;
+    }
+    public String getDisplayName() {
+        return displayName;
+    }
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+    public float getDistance() {
+        return distance;
+    }
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+    public float getxG() {
+        return xG;
+    }
+    public void setxG(float xG) {
+        this.xG = xG;
+    }
+    public float getyG() {
+        return yG;
+    }
+    public void setyG(float yG) {
+        this.yG = yG;
+    }
+    public float getzG() {
+        return zG;
+    }
+    public void setzG(float zG) {
+        this.zG = zG;
+    }
+    public float getAbsMag() {
+        return absMag;
+    }
+    public void setAbsMag(float absMag) {
+        this.absMag = absMag;
+    }
+
+    // Calculates where each star should be displayed on the grid.
+    public void render(StarMap pa)
+    {
+        float x = PApplet.map(xG, -5, 5, pa.border, pa.width - pa.border);
+        float y = PApplet.map(yG, -5, 5, pa.border, pa.height - pa.border);
+
+        
+        pa.stroke(255, 255, 0);
+        pa.line(x, y -5, x, y + 5);
+        pa.line(x-5, y, x + 5, y);
+        pa.stroke(255, 0, 0);
+        pa.noFill();
+        pa.circle(x, y, absMag);
+        pa.fill(255);
+        pa.textSize(16);
+        pa.textAlign(PApplet.LEFT, PApplet.CENTER);
+        pa.text(displayName, x + 20, y);
+    }
+
 }
--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,64 +1,134 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class StarMap extends PApplet {
 
-    int hCentre, wCentre;
+    public float border;
+    ArrayList<Star> stars = new ArrayList<Star>();
+    Star first = null, second = null;
 
     public void settings() {
         size(600, 600);
     }
-
+    
     public void setup() {
         colorMode(RGB);
-        hCentre = width / 2;
-        wCentre = width / 2;
+        loadStars(); // Accesses the CSV containing the stars and loads them into the 'stars' array list.
+        printStars(); // Prints information about the stars in the console.
+    
+        border = width * 0.1f;
     }
 
     public void draw() {
         background(0);
 
-        drawGrid();
+        drawGrid(); 
+        drawStars(); // Calls a function within the Star class which calls the stars on the grid.
+
+        // The distance can only be checked between two stars if a valid star is clicked.
+        if (first != null) 
+        {
+
+            float x = map(first.getxG(), -5, 5, border, width - border);
+            float y = map(first.getyG(), -5, 5, border, height - border);
+
+            // Distance can only be calculated if the second star is valid.
+            if (second != null)
+            {
+                float x2 = map(second.getxG(), -5, 5, border, width - border);
+                float y2 = map(second.getyG(), -5, 5, border, height - border);
+
+                stroke(255, 255, 0);
+                line(x, y, x2, y2);
+
+                float dist = dist(first.getxG(), first.getyG(), first.getzG(), second.getxG(), second.getyG(), second.getzG());
+
+                fill(255);
+                textAlign(CENTER, CENTER);
+                text("Distance between: " + first.getDisplayName() + " and " + second.getDisplayName() + " is " + dist + " parsecs", width / 2, height - (border * 0.5f));
+            }
+            else
+            {
+                stroke(255, 255, 0);
+                line(x, y, mouseX, mouseY);
+            }
+        }
+    }
+
+    /*  When a star is clicked, it is saved within the 'first' variable.
+        If a second star is selected, it is stored in the 'second' variable.
+        Otherwise, the values for each will stay null. The draw() method then finds 
+        the distance between the two. */
+    public void mouseClicked() {
+        for(Star s:stars)
+        {
+            float x = map(s.getxG(), -5, 5, border, width - border);
+            float y = map(s.getyG(), -5, 5, border, height - border);
+
+            // A safety margin is added so the user doesn't have to be exact when clicking.
+            if (dist(mouseX, mouseY, x, y) < 20)
+            {
+                if (first == null)
+                {
+                    first = s;
+                    break;
+                }
+                else if (second == null)
+                {
+                    second = s;
+                    break;
+                } 
+                else
+                {
+                    first = s;
+                    second = null;
+                    break;
+                }
+            }
+        }
     }
 
     public void drawGrid() {
-        int border = 50;
-        int X1 = 0;
-        int Y1 = 0;
-        int X2 = width - border;
-        int Y2 = height - border;
-        int textX = border - 10;
-        int textY = border / 2;
-        int textX2 = textX - border / 2;
-        int textY2 = textY + border / 2;
-        int textValue = -5;
-        String a;
-
-        stroke(150, 0, 128);
-        strokeWeight(2);
+        stroke(255, 0, 255);
+        textAlign(CENTER, CENTER);
         textSize(20);
-        textAlign(CENTER);
-
-        for (int i = 1; i <= 11; i++) {
-            a = Integer.toString(textValue);
-            Y1 = border * i;
-            text(a, textX, textY);
-            line(X1 + border, Y1, X2, Y1);
-            textValue++;
-            textX += border + 1;
-        }
-
-        Y1 = 0;
-        textValue = -5;
-
-        for (int i = 1; i <= 11; i++) {
-            a = Integer.toString(textValue);
-            X1 = border * i;
-            text(a, textX2, textY2);
-            line(X1, Y1 + border, X1, Y2);
-            textValue++;
-            textY2 += border + 1;
+        for(int i = -5; i <=5; i ++)
+        {
+            float x = map(i, -5, 5, border, width - border);
+            line(x, border, x, height - border);
+            line(border, x, width - border, x);
+            fill(255);
+            text(i, x, border * 0.5f);
+            text(i, border * 0.5f, x);
         }
     }
+
+    // Loads stars from the CSV file into the 'stars' array list.
+    void loadStars() {
+        Table table = loadTable("HabHYG15ly.csv", "header");
+        for (TableRow r : table.rows()) {
+            Star s = new Star(r);
+            stars.add(s);
+        }
+    }
+
+    void printStars() {
+        for (Star s:stars) {
+            System.out.println(s);
+        }
+    }
+
+    public void drawStars()
+    {
+        for(Star s:stars)
+        {
+            s.render(this);
+        }
+    }
+
 }
