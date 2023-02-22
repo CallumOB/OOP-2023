d10d90be9a610c4586d65b2a3f24fc92f6ba55aa
CallumOB <callumobrien2004@gmail.com>
2023-02-22 17:20:33 +0000

    Lab setup

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -6,8 +6,7 @@ import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import processing.core.PApplet;
 
-public class Audio1 extends PApplet
-{
+public class Audio1 extends PApplet {
     Minim minim;
     AudioPlayer ap;
     AudioInput ai;
@@ -20,10 +19,10 @@ public class Audio1 extends PApplet
     float smoothedAmplitude = 0;
 
     public void keyPressed() {
-		if (key >= '0' && key <= '9') {
-			mode = key - '0';
-		}
-		if (keyCode == ' ') {
+        if (key >= '0' && key <= '9') {
+            mode = key - '0';
+        }
+        if (keyCode == ' ') {
             if (ap.isPlaying()) {
                 ap.pause();
             } else {
@@ -31,20 +30,18 @@ public class Audio1 extends PApplet
                 ap.play();
             }
         }
-	}
-
-    public void settings()
-    {
-        size(1024, 1000, P3D);
-        //fullScreen(P3D, SPAN);
     }
 
-    public void setup()
-    {
+    public void settings() {
+        size(1024, 1000, P3D);
+        // fullScreen(P3D, SPAN);
+    }
+
+    public void setup() {
         minim = new Minim(this);
         // Uncomment this to use the microphone
         // ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
-        // ab = ai.mix; 
+        // ab = ai.mix;
 
         // And comment the next two lines out
         ap = minim.loadFile("heroplanet.mp3", 1024);
@@ -59,59 +56,53 @@ public class Audio1 extends PApplet
 
     float off = 0;
 
-    public void draw()
-    {
-        //background(0);
+    public void draw() {
+        // background(0);
         float halfH = height / 2;
         float average = 0;
         float sum = 0;
         off += 1;
         // Calculate sum and average of the samples
         // Also lerp each element of buffer;
-        for(int i = 0 ; i < ab.size() ; i ++)
-        {
+        for (int i = 0; i < ab.size(); i++) {
             sum += abs(ab.get(i));
         }
-        average= sum / (float) ab.size();
+        average = sum / (float) ab.size();
 
         smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
-        
+
         float cx = width / 2;
         float cy = height / 2;
 
         switch (mode) {
-			case 0:
+            case 0:
                 background(0);
-                for(int i = 0 ; i < ab.size() ; i ++)
-                {
-                    //float c = map(ab.get(i), -1, 1, 0, 255);
+                for (int i = 0; i < ab.size(); i++) {
+                    // float c = map(ab.get(i), -1, 1, 0, 255);
                     float c = map(i, 0, ab.size(), 0, 255);
                     stroke(c, 255, 255);
                     float f = ab.get(i) * halfH;
-                    line(i, halfH + f, i, halfH - f);                    
+                    line(i, halfH + f, i, halfH - f);
                 }
                 break;
-        case 1:
-            background(0);            
-            break;
+            case 1:
+                background(0);
+                break;
 
         }
-        
 
-
-        
         // Other examples we made in the class
         /*
-        stroke(255);
-        fill(100, 255, 255);        
-        
-        circle(width / 2, halfH, lerpedA * 100);
+         * stroke(255);
+         * fill(100, 255, 255);
+         * 
+         * circle(width / 2, halfH, lerpedA * 100);
+         * 
+         * circle(100, y, 50);
+         * y += random(-10, 10);
+         * smoothedY = lerp(smoothedY, y, 0.1f);
+         * circle(200, smoothedY, 50);
+         */
 
-        circle(100, y, 50);
-        y += random(-10, 10);
-        smoothedY = lerp(smoothedY, y, 0.1f);        
-        circle(200, smoothedY, 50);
-        */
-
-    }        
+    }
 }
