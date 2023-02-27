c2a6343f9fa2e4a8334ecc083bf37a83421ad6be
CallumOB <callumobrien2004@gmail.com>
2023-02-27 11:48:29 +0000

    added map function

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -12,7 +12,7 @@ public class Audio2 extends PApplet {
     AudioBuffer ab;
     
     public void settings() {
-        size(1024, 1024);
+        size(1024, 800);
     }
 
     public void setup() {
@@ -29,6 +29,15 @@ public class Audio2 extends PApplet {
         for (int i = 0; i < ab.size(); i++) {
             line(i, half, i, half + ab.get(i) * half);
         }
+
+        println(map1(5, 0, 10, 1000, 2000));
     }
 
+    float map1(float a, float b, float c, float d, float e) {
+        float percent = (c - b) / a;
+        float result = e - d; 
+        result += result / percent;
+
+        return result;
+    }
 }
