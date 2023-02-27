1d1167d822eeffa037d5e648aaecb2d6c228439e
CallumOB <callumobrien2004@gmail.com>
2023-02-27 12:00:13 +0000

    audio visualiser done

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -10,6 +10,7 @@ public class Audio2 extends PApplet {
     AudioInput ai;
     AudioPlayer ap;
     AudioBuffer ab;
+    float[] lerpedBuffer;
     
     public void settings() {
         size(1024, 800);
@@ -19,6 +20,7 @@ public class Audio2 extends PApplet {
         m = new Minim(this);
         ai = m.getLineIn(Minim.MONO, width, 44100, 16);
         ab = ai.mix;
+        lerpedBuffer = new float[width];
     }
 
     public void draw() {
@@ -27,17 +29,20 @@ public class Audio2 extends PApplet {
         float half = height / 2;
         
         for (int i = 0; i < ab.size(); i++) {
-            line(i, half, i, half + ab.get(i) * half);
+            stroke(map(i, 0, ab.size(), 0, 255), 255, 255);
+            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
+            float f = abs(lerpedBuffer[i] * half);
+            line(i, half + f, i, half - f);
         }
 
+        println(map(5, 0, 10, 1000, 2000));
         println(map1(5, 0, 10, 1000, 2000));
     }
 
     float map1(float a, float b, float c, float d, float e) {
         float percent = (c - b) / a;
         float result = e - d; 
-        result += result / percent;
 
-        return result;
+        return result + (result / percent);
     }
 }
