24552a59cef719847f89483b6e33c919429f4369
CallumOB <callumobrien2004@gmail.com>
2023-02-27 12:49:33 +0000

    added frequency display

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -1,7 +1,7 @@
 package ie.tudublin;
 
 import ddf.minim.*;
-import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
 import processing.core.PApplet;
 
 public class Audio2 extends PApplet {
@@ -11,6 +11,7 @@ public class Audio2 extends PApplet {
     AudioPlayer ap;
     AudioBuffer ab;
     float[] lerpedBuffer;
+    FFT fft;
     
     public void settings() {
         size(1024, 800);
@@ -22,6 +23,7 @@ public class Audio2 extends PApplet {
         ai = m.getLineIn(Minim.MONO, width, 44100, 16);
         ab = ai.mix;
         lerpedBuffer = new float[width];
+        fft = new FFT(width, 44100);
     }
 
     public void draw() {
@@ -35,6 +37,19 @@ public class Audio2 extends PApplet {
             float f = abs(lerpedBuffer[i] * half);
             line(i, half + f, i, half - f);
         } // end for 
+
+        fft.forward(ab);
+        stroke(255);
+        int highest = 0;
+        for (int i = 0; i < fft.specSize() / 2; i++) {
+            line(i * 2, height, i * 2, height - fft.getBand(i) * 5.0f);
+            if (fft.getBand(i) > highest) {
+                highest = i;
+            }
+        }
+        float freq = fft.indexToFreq(highest);
+        fill(255);
+        text("Freq: " + freq, 10, 50);
     }
 
     float map1(float a, float b, float c, float d, float e) {
